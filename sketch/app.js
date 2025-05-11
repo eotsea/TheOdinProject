cd72dbd762b160cb11d484e520d3c224b55af539
Muhammed Uğur <eotsea@DESKTOP-91DT3PA.>
2025-05-11 16:53:58 +0300

    AdminDashboard

--- sketch/app.js
+++ sketch/app.js
@@ -1,6 +1,22 @@
+"use strict";
 const container = document.querySelector(".container");
 const input = document.getElementById("input");
 const generateButton = document.querySelector("button");
+let color;
+let mouse = false;
+const colorPickerBar = (value) => {
+  const colorPicker = document.querySelector(".colorPicker");
+  for (let i = 0; i < value; i++) {
+    const li = document.createElement("li");
+    li.innerHTML = `<div class ="colorBox"></div>`;
+    li.style.backgroundColor = randomRGB();
+    colorPicker.appendChild(li);
+    li.addEventListener("click", () => {
+      color = li.style.backgroundColor;
+    });
+  }
+};
+
 const randomRGB = () => {
   const x = Math.floor(Math.random() * 256);
   const y = Math.floor(Math.random() * 256);
@@ -18,15 +34,24 @@ const cellMaker = (cellSize) => {
 const gridMaker = (count) => {
   container.innerHTML = "";
   const cellSize = Math.floor(500 / count);
-  for (let i = 0; i < count*count; i++) {
+  for (let i = 0; i < count * count; i++) {
     container.append(cellMaker(cellSize));
   }
 };
 
+container.addEventListener("mousedown", (e) => {
+  mouse = true;
+});
+
+container.addEventListener("mouseup", (e) => {
+  mouse = false;
+});
 container.addEventListener("mouseover", (event) => {
   console.log("on hover");
-  if (event.target.classList.contains("grid-cell")) {
-    event.target.style.backgroundColor = randomRGB();
+  if(mouse){
+    if (event.target.classList.contains("grid-cell")) {
+      event.target.style.backgroundColor = color;
+    }
   }
 });
 
@@ -40,5 +65,6 @@ generateButton.addEventListener("click", () => {
     parseInt(input.value) > 0
   ) {
     gridMaker(parseInt(input.value));
+    colorPickerBar(input.value);
   }
 });
